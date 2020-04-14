3902c2740701c7d12acc0192ccae746380853855
Sarah Carter <sarahmarie.carter@yahoo.com>
2020-04-14 14:00:40 -0700

    added webpack.config.js, service-worker.js, and package.json to public folder. Ran npm run build to build web app manifest

---
+++ public/webpack.config.js
@@ -0,0 +1,43 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
+const config = {
+  entry: "./index.html",
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js"
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new WebpackPwaManifest({
+      name: "Budget App",
+      short_name: "Budget App",
+      description: "An application that allows you to track your budget.",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      "theme-color": "#ffffff",
+      start_url: "/",
+      icons: [{
+        src: path.resolve("./icons/icon-512x512.png"),
+        sizes: [96, 128, 192, 256, 384, 512],
+        destination: path.join("assets", "icons")
+      }]
+    })
+  ]
+};
+
+module.exports = config;
